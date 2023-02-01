dee825c3140f63023fe7073c3d732d98bad76bfe
Jesus Ruiz <javier.ruiz2506@gmail.com>
2023-02-01 18:59:52 -0400

    archivo database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
-const express = require('express');
+import express from "express";
+import morgan from "morgan";
+import session from "express-session";
+import cors from 'cors'
 const config = require('./config');
 
 const app = express();
-
+app.use(cors())
 app.set('port', config.app.port)
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json())
+
+// Session
+app.use(session({
+    secret: 'webslesson',
+    resave: true,
+    saveUninitialized: true
+}));
 
 module.exports = app;
